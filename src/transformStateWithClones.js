744ba8f04cd8935a27a18098d7a8d35f6467b4f9
Vitalii Podoliuk <wese7244@gmail.com>
2025-09-21 20:34:58 +0300

    Fix

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -9,27 +9,27 @@
 function transformStateWithClones(state, actions) {
   const transformations = [];
   const midState = { ...state };
-  let i = 0;
+  let index = 0;
 
   for (const action of actions) {
     if (action.type === 'addProperties') {
       for (const key in action.extraData) {
         midState[key] = action.extraData[key];
       }
-      transformations[i] = { ...midState };
-      i++;
+      transformations[index] = { ...midState };
+      index++;
     } else if (action.type === 'removeProperties') {
       for (const key of action.keysToRemove) {
         delete midState[key];
       }
-      transformations[i] = { ...midState };
-      i++;
+      transformations[index] = { ...midState };
+      index++;
     } else if (action.type === 'clear') {
       for (const key in midState) {
         delete midState[key];
       }
-      transformations[i] = { ...midState };
-      i++;
+      transformations[index] = { ...midState };
+      index++;
     }
   }
 
